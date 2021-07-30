991d3eaa7c15174411cb0b4f5039ca1d972c32cb
照井拓朗 <>
2021-07-30 20:47:49 +0900

    不要な記述削除

--- www/PackTweet/application/public/js/tweet_btn.js
+++ www/PackTweet/application/public/js/tweet_btn.js
@@ -1,12 +1,4 @@
 $(function() {
-  // $('#search-input-box').on('input', function() {
-  //   var input = $(this).val();
-  //   if(input) {
-  //     $('#search-btn').prop('disabled', false);
-  //   } else {
-  //     $('#search-btn').prop('disabled', true);
-  //   }
-  // });
   $('.tweet-textarea').on('input', function() {
     var input = $(this).val();
     if(input) {
